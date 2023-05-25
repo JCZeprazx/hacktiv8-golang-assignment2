33dd3b45ee4b294497f2a891d3c53dc91b4f288e
JCZepazx <jcpsaimens@gmail.com>
2023-05-25 22:46:26 +0700

    entity

---
+++ entity/order.go
@@ -0,0 +1,13 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Order struct {
+	gorm.Model
+	OrderedAt    time.Time `gorm:"default:now()"`
+	CustomerName string    `gorm:"not null"`
+	Item         []Item
+}
